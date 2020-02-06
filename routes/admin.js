919b048e0918c0282f116ea6eb126bd173be9872
Tobechukwu Akabogu (LLC) <TO20039369@wipro.com>
2020-02-05 19:10:59 -0600

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const adminController = require('../controllers/admin');
+
+
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.getPostAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
